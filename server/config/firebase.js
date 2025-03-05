bc67e95c20c51489a56a05a924b13de5d3303293
daksh <dakshtitarmare2006@gmail.com>
2025-03-05 19:44:56 +0530

    fireabaseSDK-addedtogitignore

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./traveltinder-e2e1f-firebase-adminsdk-fbsvc-c84b6886ee.json");
+const serviceAccount = require("./travel.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
